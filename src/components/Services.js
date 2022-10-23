9bb0d5d9332f3c00869d6f7d1c85e4056d64cc2e
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2022-10-23 23:09:37 +0700

    change conent

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,57 +7,69 @@ function Services() {
             <div className="col-md-12">
                 <div className="row">
                     <div className="col-md-4 mb-2">
-                        <Card title="Waltz" img="card1.png" text="Morbi eget neque risus. Duis erat quam, porta quis enim id, venenatis blandit nunc. " />
+                        <Card title="Waltz" img="card1.png" text="Waltz adalah suatu jenis tarian ruangan (ballroom dance) dan tarian rakyat dalam ketukan 3/4, dilakukan terutama dalam posisi tertutup (closed position). Gerakan dasar utama dari suatu wals adalah suatu putaran penuh dengan dua tahap dengan tiga langkap per tahap.
+
+Waltz menjadi populer di Wina sekitar 1780-an dan menyebar ke berbagai negara lainnya pada tahun-tahun berikutnya. Waltz dan posisi tertutupnya menjadi contoh untuk penciptaan berbagai tarian ruangan lainnya. Selanjutnya, banyak tipe waltz berkembang, termasuk beberapa tarian rakyat dan ruangan lainnya." />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="Slow Fox" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
+                    <Card title="Slow Fox" img="card2.png" text="Slow Fox berasal dari awal 1900-an di Amerika sebagai Ragtime dan awalnya menari dengan sangat cepat. Langkah-langkahnya sebagian besar diambil dari jalan kastil dan satu langkah dari Amerika Utara. Harry Fox adalah seorang bintang di Ziegfeld Follies Theatre di Broadway di New York. Selama pertunjukan pada tahun 1914, ia menari dalam musik ragtime populer saat itu, sebuah tarian baru yang unik, Trot (tarian trot). Hit Harry Fox Trot dengan jelas dan cepat mendapat banyak penggemar.
+Hari ini, bagaimanapun, ia dimainkan perlahan." />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="Tango" img="card3.png" text="Phasellus suscipit nibh at nisi finibus vestibulum sit amet vitae massa. Suspendisse non est eget elit pulvinar consectetur nec non sapien." />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-4 mb-2">
-                        <Card title="Quick Step" img="card1.png" text="Morbi eget neque risus. Duis erat quam, porta quis enim id, venenatis blandit nunc. " />
-                    </div>
-                    <div className="col-md-4 mb-2">
-                    <Card title="Viennese Waltz" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
-                    </div>
-                    <div className="col-md-4 mb-2">
-                    <Card title="Cha Cha Cha" img="card3.png" text="Phasellus suscipit nibh at nisi finibus vestibulum sit amet vitae massa. Suspendisse non est eget elit pulvinar consectetur nec non sapien." />
+                    <Card title="Tango" img="card3.png" text="Tango adalah tarian pasangan dan tarian sosial yang berasal dari tahun 1880-an di sepanjang Río de la Plata , perbatasan alami antara Argentina dan Uruguay . Tango lahir di daerah pelabuhan miskin di negara-negara ini sebagai hasil dari kombinasi perayaan Rioplatense Candombe , Habanera Spanyol-Kuba , dan Milonga Argentina . Tango sering dipraktekkan di rumah bordil dan bar pelabuhan, di mana pemilik bisnis mempekerjakan band untuk menghibur pelanggan mereka. Tango kemudian menyebar ke seluruh dunia. Banyak variasi tarian ini saat ini ada di seluruh dunia." />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-4 mb-2">
-                        <Card title="Jive" img="card1.png" text="Morbi eget neque risus. Duis erat quam, porta quis enim id, venenatis blandit nunc. " />
+                        <Card title="Quick Step" img="card1.png" text="Quickstep adalah tarian ringan dari tarian ballroom standar . Gerakan tariannya cepat dan mengalir kuat dan ditaburi sinkopasi . Melodi upbeat quickstep yang ditarikan membuatnya cocok untuk acara formal maupun informal. Quickstep dikembangkan pada 1920-an di New York City dan pertama kali ditarikan oleh orang kulit hitam Amerika. Asal-usulnya dalam kombinasi dari foxtrot lambat dikombinasikan dengan Charleston , sebuah tarian yang merupakan salah satu pendahulu dari apa yang sekarang disebut tarian ayunan" />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="Paso Doble" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
+                    <Card title="Viennese Waltz" img="card2.png" text="Viennese Waltz atau waltz Wina adalah genre dansa ballroom . Setidaknya empat arti yang berbeda diakui. Dalam pengertian historis pertama, nama mungkin merujuk ke beberapa versi waltz , termasuk waltz paling awal dilakukan dalam dansa ballroom, menari dengan musik waltz Wina.
+
+
+penari
+Apa yang sekarang disebut waltz Wina adalah bentuk asli dari waltz. Itu adalah dansa ballroom pertama yang dilakukan dalam posisi closed hold atau waltz. Tarian yang dikenal sebagai waltz sebenarnya adalah waltz Inggris atau slow waltz, ditarikan sekitar 90 ketukan per menit dengan 3 ketukan ke bar (standar internasional 30 langkah per menit ), sedangkan waltz Wina ditarikan sekitar 180 ketukan. denyut (58-60 langkah) per menit. Namun sampai hari ini, di Jerman, Austria, Skandinavia, dan Prancis, kata-kata Walzer (Jerman), vals (Denmark, Norwegia, dan Swedia), dan valse (Prancis) masih secara implisit merujuk pada tarian asli dan bukan waltz lambat." />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="Rumba Dance" img="card3.png" text="Phasellus suscipit nibh at nisi finibus vestibulum sit amet vitae massa. Suspendisse non est eget elit pulvinar consectetur nec non sapien." />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-4 mb-2">
-                        <Card title="Samba" img="card1.png" text="Morbi eget neque risus. Duis erat quam, porta quis enim id, venenatis blandit nunc. " />
-                    </div>
-                    <div className="col-md-4 mb-2">
-                    <Card title="Traditional Dance" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
-                    </div>
-                    <div className="col-md-4 mb-2">
-                    <Card title="Synchronize Dance" img="card3.png" text="Phasellus suscipit nibh at nisi finibus vestibulum sit amet vitae massa. Suspendisse non est eget elit pulvinar consectetur nec non sapien." />
+                    <Card title="Cha Cha Cha" img="card3.png" text="Cha - cha-cha (juga disebut cha-cha ), adalah tarian asal Kuba. Ini ditarikan dengan musik dengan nama yang sama yang diperkenalkan oleh komposer dan pemain biola Kuba Enrique Jorrin pada awal 1950-an. Ritme ini dikembangkan dari danzón-mambo . Nama tarian ini adalah onomatopoeia yang berasal dari suara langkah kaki penari yang menyeret ketika mereka menari dua langkah cepat berturut-turut (benar, pada hitungan keempat setiap langkah) yang menjadi ciri tarian.
+Pada awal 1950-an, Enrique Jorrín bekerja sebagai pemain biola dan komposer dengan grup charanga Orquesta América . Kelompok ini tampil di ruang dansa di Havana di mana mereka memainkan danzón , danzonete, dan danzon-mambo untuk orang banyak yang berorientasi pada tarian. Jorrín memperhatikan bahwa banyak penari di pertunjukan ini mengalami kesulitan dengan ritme sinkopasi dari danzón-mambo. Untuk membuat musiknya lebih menarik bagi para penari, Jorrín mulai mengaransemen lagu-lagu di mana melodinya ditandai dengan kuat pada downbeat pertama dan ritmenya kurang selaras . Ketika Orquesta América menampilkan komposisi baru ini di Silver Star Club di Havana, diketahui bahwa para penari telah berimprovisasi dengan tiga langkahdalam gerak kaki mereka menghasilkan suara cha-cha-cha. Dengan demikian, gaya baru ini kemudian dikenal sebagai cha-cha-chá dan dikaitkan dengan tarian di mana penari melakukan tiga langkah." />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-4 mb-2">
-                        <Card title="Line Dance" img="card1.png" text="Morbi eget neque risus. Duis erat quam, porta quis enim id, venenatis blandit nunc. " />
+                        <Card title="Jive" img="card1.png" text="Jive adalah gaya tarian yang berasal dari Amerika Serikat dari Afrika Amerika pada awal 1930-an. Nama tarian ini berasal dari nama sebuah bentuk bahasa gaul Afrika-Amerika , yang dipopulerkan pada tahun 1930-an oleh penerbitan kamus oleh Cab Calloway , pemimpin band dan penyanyi jazz terkenal. Dalam dansa ballroom kompetisi , jive sering dikelompokkan dengan tarian ballroom yang terinspirasi Latin, meskipun akarnya didasarkan pada tarian ayunan dan bukan tarian Latin." />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="Breakdance" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
+                    <Card title="Paso Doble" img="card2.png" text="Paso doble, atau pasodoble, adalah tarian ballroom Latin. “Paso doble” mungkin berasal dari Prancis atau Spanyol—istilah “paso doble” berarti “langkah ganda” atau “dua langkah” dalam bahasa Spanyol—karena musik paso doble yang bergerak cepat mengiringi langkah cepat pawai militer di keduanya. negara. Tarian ini juga memiliki hubungan dengan adu banteng: Musik paso doble yang bergerak cepat awalnya disajikan sebagai tema pengantar untuk matador Spanyol yang memasuki ring. Gerakan matador dan banteng mengilhami tarian, menurut legenda Spanyol." />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="Hip Hop Dance" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
+                    <Card title="Rumba Dance" img="card3.png" text="Rhumba, juga dikenal sebagai ballroom rumba, adalah genre musik dan tarian ballroom yang muncul di Pantai Timur Amerika Serikat selama tahun 1930-an. Ini menggabungkan musik band besar Amerika dengan ritme Afro-Kuba, terutama putra cubano, tetapi juga conga dan rumba. Meskipun mengambil namanya dari yang terakhir, rumba ballroom sangat berbeda dari rumba Kuba baik dalam musik maupun tariannya. Oleh karena itu, penulis lebih memilih ejaan kata Amerikanisasi (rhumba) untuk membedakannya." />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4 mb-2">
+                        <Card title="Samba" img="card1.png" text="Istilah samba awalnya merujuk pada salah satu dari beberapa tarian duet Latin yang berasal dari Kongo dan Angola. Saat ini Samba adalah bentuk tarian yang paling umum di Brasil, dan mencapai puncak pentingnya selama festival Karnaval. Sebenarnya ada satu set tarian, bukan satu tarian, yang mendefinisikan adegan tarian Samba di Brasil; Namun, tidak ada satu pun tarian yang dapat diklaim secara pasti sebagai gaya Samba yang asli. Selain Samba Brasil, gaya utama Samba adalah ballroom Samba, yang berbeda secara signifikan." />
+                    </div>
+                    <div className="col-md-4 mb-2">
+                    <Card title="Traditional Dance" img="card2.png" text="Tarian yang telah berkembang dari masa ke masa yang telah melewati waktu yang cukup lama di suatu daerah, adat, atau etnik tertentu." />
+                    </div>
+                    <div className="col-md-4 mb-2">
+                    <Card title="Synchronize Dance" img="card3.png" text="Tarian yang disinkronkan" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4 mb-2">
+                        <Card title="Line Dance" img="card1.png" text="Dance sendiri adalah olah tubuh dengan sentuhan koreografi gerakan tari. Gerakan dari senam Line Dance diadaptasi dari langkah-langkah dasar berbagai jenis dansa, seperti rumba, salsa, cha-cha, merengue, waltz, hip-hop, dan lainnya." />
+                    </div>
+                    <div className="col-md-4 mb-2">
+                    <Card title="Breakdance" img="card2.png" text="Breakdance, breaking, b-boying atau b-girling adalah gaya tari jalanan yang muncul sebagai bagian dari gerakan hip hop di antara African American dan anak muda dari Puerto Rico yang dilakukan di bagian selatan New York City yang brutal pada tahun 1970. Pada Umumnya tarian ini diiringi lagu hip hop,rap, atau lagu remix (lagu yang di aransemen ulang).
+
+Seorang breakdancer, breaker, b-boy atau b-girl adalah orang yang memainkan gerakan breakdance." />
+                    </div>
+                    <div className="col-md-4 mb-2">
+                    <Card title="Hip Hop Dance" img="card2.png" text="Tari hip hop adalah berbagai gaya tari jalanan yang terutama dilakukan untuk musik hip hop atau yang telah berkembang sebagai bagian dari budaya hip hop. Hal ini dipengaruhi oleh berbagai gaya yang diciptakan pada 1970-an dan dipopulerkan oleh kru tari di Amerika Serikat. Acara televisi Soul Train dan film tahun 1980-an Breakin', Beat Street, dan Wild Style menampilkan kru dan gaya tarian ini pada tahap awal mereka; oleh karena itu, memberikan paparan arus utama tari hip-hop.
+
+Industri tari merespons dengan versi komersial hip-hop berbasis studio—kadang-kadang disebut gaya baru dan gaya tari jazz yang dipengaruhi hip-hop disebut jazz-funk. Penari yang terlatih secara klasik mengembangkan gaya studio ini untuk menciptakan koreografi dari tarian hip-hop yang dilakukan di jalanan. Karena perkembangan ini, tari hip-hop dipraktekkan di studio tari dan ruang terbuka." />
                     </div>
                 </div>
             </div>
